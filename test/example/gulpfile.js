c10a231b4d5ab6682930df55ab2d702c954d041a
Caleb Brewer <calebthebrewer@gmail.com>
2015-04-14 00:44:08 -0500

    updating example for newer meathods

--- test/example/gulpfile.js
+++ test/example/gulpfile.js
@@ -3,6 +3,9 @@ var selectors = require('../../index');
 
 gulp.task('default', function() {
 	return gulp.src(['index.html', 'style.css', 'script.js'])
-		.pipe(selectors())
+		.pipe(selectors.run({
+			'js-strings': ['js']
+		}))
+		.pipe(selectors.info())
 		.pipe(gulp.dest('./dist'));
 });
