1d896b0bcdb3d668967e0ec866de1ae8ee4bfb0e
hashworks <admin@hashworks.net>
2015-04-16 11:04:44 +0200

    Suffix class test cases by  { so they match the new selectors

--- test/expressions/expressions.class-selector.test.js
+++ test/expressions/expressions.class-selector.test.js
@@ -4,13 +4,13 @@ var vows = require('vows'),
 
 vows.describe('Expressions: class selectors').addBatch({
 	'An class selector': {
-		topic: '.selector'.match(expressions.classSelector),
+		topic: '.selector {'.match(expressions.classSelector),
 		'should return one match': function(topic) {
 			assert.equal(topic.length, 1);
 		}
 	},
 	'Nested class selectors': {
-		topic: '.selector .child'.match(expressions.classSelector),
+		topic: '.selector .child {'.match(expressions.classSelector),
 		'should return two matches': function(topic) {
 			assert.equal(topic.length, 2);
 		}
@@ -22,13 +22,13 @@ vows.describe('Expressions: class selectors').addBatch({
 		}
 	},
 	'A pseudo selector': {
-		topic: ':hover'.match(expressions.classSelector),
+		topic: ':hover {'.match(expressions.classSelector),
 		'should not return a match': function(topic) {
 			assert.equal(topic, null);
 		}
 	},
 	'A tag selector': {
-		topic: 'body'.match(expressions.classSelector),
+		topic: 'body {'.match(expressions.classSelector),
 		'should return a match': function(topic) {
 			assert.equal(topic, null);
 		}
